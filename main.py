1ed6f4f55460c1d29235b9f01a7c2a2c8a7bdded
bitPravda <hbgenius_8@hotmail.com>
2025-04-15 10:25:32 +0800

    fix host issue

--- main.py
+++ main.py
@@ -77,4 +77,4 @@ def read_chat(chat_user_id: str):
   
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=3333)
+    uvicorn.run(app, host="0.0.0.0", port=3333)
